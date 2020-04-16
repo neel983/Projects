063a2fd85c1c507684fa5c86a651039a101cddea
Nilanjan Dutta <nilanjan.dutta1998@gmail.com>
2020-04-16 17:42:31 +0530

    Third commit

--- ConnectionChecker.cmd
+++ ConnectionChecker.cmd
@@ -1,10 +1,10 @@
 @echo off
 del Hi.txt
 color 0a
+set /p address=<Readme.md
 set /p t=Set waiting time in seconds:
 :start
-
-ping -t 10.15.109.1 -n 1 >pingResult.txt
+ping -t %address% -n 1 >pingResult.txt
 FOR /F "tokens=2 skip=2" %%v IN (pingResult.txt) DO echo %%v>>Hi.txt
 set /p d=<Hi.txt
 if "%d%" == "from" goto Found
@@ -18,5 +18,5 @@ cls
 goto start
 
 :Found
-start wmplayer.exe "./CoonectionEstablished.mp3"
+CoonectionEstablished.mp3
 exit
