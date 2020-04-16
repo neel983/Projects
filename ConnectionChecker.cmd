7b6835c92567c632c1036cfa8626994c82a5de26
Nilanjan Dutta <nilanjan.dutta1998@gmail.com>
2020-04-16 17:08:47 +0530

    First Commit

---
+++ ConnectionChecker.cmd
@@ -0,0 +1,22 @@
+@echo off
+del Hi.txt
+color 0a
+set /p t=Set waiting time in seconds:
+:start
+
+ping -t 10.15.109.1 -n 1 >pingResult.txt
+FOR /F "tokens=2 skip=2" %%v IN (pingResult.txt) DO echo %%v>>Hi.txt
+set /p d=<Hi.txt
+if "%d%" == "from" goto Found
+goto error
+exit
+
+:error
+del Hi.txt
+timeout /T %t% /NOBREAK
+cls
+goto start
+
+:Found
+start wmplayer.exe "C:\Users\USER\Downloads\Music\Top_Friendship_Songs_-_Bollywood_Friendship_Day_Sp.mp3"
+exit
